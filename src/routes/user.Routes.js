4f5d692bb62016540b9e099f2eca464e9f131a2c
Tachobo <wilmerardila2001@gmail.com>
2026-04-10 23:41:12 -0500

    feat: Se integro middleware validateSchema en rutas de usuarios (POST, PUT, PATCH), tambien se instalo la libreria ZOD

--- src/routes/user.Routes.js
+++ src/routes/user.Routes.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import { getUsers, createUser, updateUser, updateUserPartial, deleteUser } from '../controller/users.controller.js';
+import { validateSchema } from '../middlewares/validator.middleware.js';
+import { userSchema, userPartialSchema } from '../schemas/user.schema.js';
 
 const usersRouter = express.Router();
 
 usersRouter.get('/', getUsers);
 usersRouter.get('/:id', getUsers);
-usersRouter.post('/', createUser);
-usersRouter.put('/:id', updateUser);
-usersRouter.patch('/:id', updateUserPartial);
+usersRouter.post('/', validateSchema(userSchema), createUser);
+usersRouter.put('/:id', validateSchema(userPartialSchema), updateUser);
+usersRouter.patch('/:id', validateSchema(userPartialSchema), updateUserPartial);
 usersRouter.delete('/:id', deleteUser);
 
 export default usersRouter;
